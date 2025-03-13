a5b9b4df68598cd85fb8310441875f785edc3f62
prasanna-kothalkar1 <prasanna.kothalkar1@gmail.com>
2025-03-12 23:44:59 -0500

    Added InformedSearch

---
+++ InformedSearch/src/PathInfo.java
@@ -0,0 +1,40 @@
+
+/***
+ * Keeps path information for each state
+ *
+ */
+public class PathInfo {
+
+	private State state;
+	
+	/* Represents the f(n) value computed */
+	private double fnValue;
+	
+	private String pathFromStart;
+	
+	private double costFromStart;
+	
+	public PathInfo(State s, double fnval, String path, double cost) {
+		state = s;
+		fnValue = fnval;
+		pathFromStart = new String(path);
+		costFromStart = cost;
+	}
+
+	State getState() {
+		return state;
+	}
+	
+	double getFnValue() {
+		return fnValue;
+	}
+	
+	String getPathFromStart() {
+		return pathFromStart;
+	}
+	
+	double getCostFromStart() {
+		return costFromStart;
+	}
+}
+
